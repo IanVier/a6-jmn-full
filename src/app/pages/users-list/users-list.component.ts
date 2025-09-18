d0a404187ad048fcb6a66da5e777b747267b7c82
Javier Morales <eyjavi@gmail.com>
2025-09-18 22:30:26 +0200

    firs api conection

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IApiResponse, IUsers } from '../../interfaces/iusers.interface';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
+  userService = inject(UserService)
+  userData: IUsers[] = []
+
+  ngOnInit() {
+    this.uploadData()
+  }
+
+  async uploadData () {
+    try {
+        const response: IApiResponse = await this.userService.getAll()
+        this.userData = response.results
+    }
+    catch (error) {
+      alert(error)
+    }
+  }
+
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { IApiResponse, IUsers } from '../interfaces/iusers.interface';
+import { IApiResponse } from '../interfaces/iusers.interface';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class IUserService {
+export class UserService {
   private endPoint = 'https://peticiones.online/api/users'
   private httpClient = inject(HttpClient)
 
-  getAll(): Promise<IApiResponse[]> {
-    return lastValueFrom(this.httpClient.get<IApiResponse[]>(this.endPoint))
+  getAll(): Promise<IApiResponse> {
+    return lastValueFrom(this.httpClient.get<IApiResponse>(this.endPoint))
   }
 
 
