33c8861c3c3bddc0db2f574ae3f06a9aa9b2f5dd
Javier Morales <eyjavi@gmail.com>
2025-09-18 08:13:03 +0200

    interface and service created

--- src/app/services/iuser.service.ts
+++ src/app/services/iuser.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IApiResponse, IUsers } from '../interfaces/iusers.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IUserService {
-  
+  private endPoint = 'https://peticiones.online/api/users'
+  private httpClient = inject(HttpClient)
+
+  getAll(): Promise<IApiResponse[]> {
+    return lastValueFrom(this.httpClient.get<IApiResponse[]>(this.endPoint))
+  }
+
+
 }
