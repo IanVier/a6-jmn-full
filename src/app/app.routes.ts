bcf9028e9090a2999585f13dea8a82be1ef47c29
Javier Morales <eyjavi@gmail.com>
2025-09-19 01:23:30 +0200

    user view and list works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { UsersListComponent } from './pages/users-list/users-list.component';
 import { FormComponent } from './pages/form/form.component';
 import { C404Component } from './pages/c404/c404.component';
-import { UsersViewComponent } from './paes/users-view/users-view.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+
 
 export const routes: Routes = [
     {path: "", pathMatch: 'full', redirectTo: "home"},
     {path: "home", component: UsersListComponent},
     {path: "newuser", component: FormComponent}, 
     {path: "user/edit/:_id", component: FormComponent },
-    {path: 'user/:id', component: UsersViewComponent},
+    {path: 'user/:_id', component: UserViewComponent},
     {path: '**', component: C404Component}
 ];
